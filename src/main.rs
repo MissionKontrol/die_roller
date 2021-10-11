df7e71486dfc119055f6f1177edc2222c541c9c2
Peter <sceptix@gmail.com>
2021-10-11 17:00:34 -0400

    parseing done

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,89 @@
-fn main() {
-    let requested_roll = "1d12";
-    
-    let roll_result = roll_request(requested_roll);
-}
+use rand::Rng;
 
-fn roll_request(&requested_roll) -> {
-    let roll_request_list = parse_roll_request(&requested_roll);
-    for roll in roll_request_list {
-        roll_result = roll_
+fn main() {
+    let requested_roll = "1d12+d4 +8+ 2d6".to_string();
+
+    let parsed_roll = parse_request(&requested_roll);
+    let parsed_roll_list = parsed_roll.unwrap();
+
+    for token in parsed_roll_list.request_list {
+        if let RequestToken::Dice(_) = token {
+            let roll_result = roll(token);
+        }
     }
 }
 
-fn parse_roll_request(request_string: &str) -> Option<T>{
+fn roll(roll_request_token: RequestToken){
 
-    None
-}
-
-struct Die {
-    sides: u8,
-}
-
-trait dice {
-    fn simple_roll() -> Option<RollResult>;
-    fn complex_roll() -> Option<RollResultList>;
-}
-
-struct SimpleRoll {
-    die: Die,
-    roll_result: u16,
 }
 
 struct RollResult {
-    total_roll_result: u16,
-    
+    total: u16,
+    // roll_list: Vec<T>,
 }
 
-struct RollRequestList {
-    number_unique_dice: u16,
-    roll_requests: Vec<roll_request_composition>,
+#[derive(Debug, Clone)]
+struct DiceThrowDescription {
+    number_of_dice: u16,
+    number_of_die_sides: u8,
 }
 
-struct RollRequestComposition {
-    number_rolls: u8,
-    die_size: u8,
-    roll_options: RollOptions,
+#[derive(Debug, Clone)]
+struct DiceThrowModifier {
+    operator: ModifierOperators,
+    value: u8,
 }
 
-enum RollOptions {
-    Once,
-    ReRollOnceLessThan: u8,
-    ReRollManyLessThan: u8,
-    ReRollOnceGreaterThan: u8,
-    ReRollManyGreaterThan: u8,
+#[derive(Debug, Clone)]
+struct RollRequest {
+    request_list: Vec<RequestToken>,
+}
+
+#[derive(Debug, Clone)]
+enum ModifierOperators {
+    Add,
+    _Subtract,
+    _Multiply,
+    _Divide,
+}
+
+#[derive(Debug, Clone)]
+enum RequestToken {
+    Dice(DiceThrowDescription),
+    Modifier(DiceThrowModifier),
+    Error,
+}
+
+
+fn roll_request(request_string: &str) -> Option<RollResult> {
+    println!("{}:  roll_request", request_string);
+    None
+}
+
+fn parse_request(request_string: &str) -> Option<RollRequest> {
+    let tokens = tokenize(request_string.to_owned());
+    Some(RollRequest{request_list:tokens})
+}
+
+fn tokenize(mut request_string: String) -> Vec<RequestToken> {
+    request_string.retain(|c| !c.is_whitespace());
+    let general_token_list: Vec<&str> = request_string.rsplit('+').collect();
+    let mut request_token_list: Vec<RequestToken> = Vec::new();
+    for general_token in general_token_list {
+        let request_token = make_request_token(general_token);
+        request_token_list.push(request_token);
+    }
+    request_token_list
+}
+
+fn make_request_token(in_token: &str) -> RequestToken {
+    let token_parts = in_token.split('d').collect::<Vec<&str>>();
+
+    match token_parts.len() {
+        1 => return RequestToken::Modifier(DiceThrowModifier {operator: ModifierOperators::Add,
+            value: token_parts[0].parse().unwrap()}),
+        2 => return RequestToken::Dice(DiceThrowDescription {number_of_dice: token_parts[0].parse().unwrap_or(1),
+            number_of_die_sides: token_parts[1].parse().unwrap()}),
+        _ => return RequestToken::Error,
+        };
 }
